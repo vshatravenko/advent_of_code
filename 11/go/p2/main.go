19e58e6412ae0cd0b96bc1f7d70927c1b8e2f69f
Valentine Shatravenko <vshatravenko@gmail.com>
2023-10-08 22:15:33 -0400

    add go solution for day 11 part 2

--- 11/go/p2/main.go
+++ 11/go/p2/main.go
@@ -5,12 +5,36 @@ package main
 	1. Parse the input for each monkey and put it in a struct
 	2. Perform the simulation while keeping a counter of ops per monkey
 	3. Return the top two counters multiplied
+
+	The main change now is that the simulation spans 10000 rounds
+    and the worry levels per item are not divided by 3, so int would overflow
+	math/big.Int is used instead
+*/
+
+/*
+	Thoughts
+	Currently, even with math/big in play, the simulation stalls at the first ~10%
+	just because of the fact how huge the numbers are, with operations
+	starting to take multiple seconds.
+	All these computations are done for just one thing - determining whether an item is
+	divisible by a given number and than transforming it by adding or multiplying it
+	I suppose that we could circumvent the operational complexity of big numbers by
+	either checking the divisibility using a set of operations on a number's integers,
+	or by keeping a set of factors (e.g. (((5 * 6) + 10) * 6) + 11) etc. and then unwrapping it
+	but it sounds a bit tough
+	Also, I may be approaching the problem from the wrong direction, and divisibility
+	shouldn't even come into play. Maybe, there's a more efficient way of determining
+	the next monkey.
+
+	A possible clue - all the divisibility numbers are prime
+
+	Maybe, we could keep the list of operands and check their individual adherence to divisibility?
 */
 
 import (
 	"bufio"
 	"fmt"
-	"math"
+	"math/big"
 	"os"
 	"strconv"
 	"strings"
@@ -18,15 +42,15 @@ import (
 
 const (
 	defaultInputPath = "input.txt"
-	itemDivider      = 3 // used for dividing item after every operation
+	simulationLen    = 10000
 )
 
 type monkey struct {
-	items      []int
+	items      []*big.Int
 	opElems    []string
-	divisible  int
+	divisor    *big.Int
 	nextMonkey map[bool]int
-	opCount    int
+	opCount    *big.Int
 }
 
 func main() {
@@ -46,7 +70,6 @@ func main() {
 		fmt.Printf("err: could not parse input: %s\n", err)
 		os.Exit(1)
 	}
-	printMonkeys(monkeys)
 
 	simulate(monkeys)
 
@@ -74,7 +97,7 @@ func parseInput(f *os.File) ([]*monkey, error) {
 }
 
 func parseMonkey(scanner *bufio.Scanner) (*monkey, error) {
-	res := &monkey{}
+	res := &monkey{opCount: big.NewInt(0)}
 
 	if ok := scanner.Scan(); !ok || scanner.Text() == "" {
 		return nil, fmt.Errorf("monkey input cut too early")
@@ -93,11 +116,11 @@ func parseMonkey(scanner *bufio.Scanner) (*monkey, error) {
 	if ok := scanner.Scan(); !ok || scanner.Text() == "" {
 		return nil, fmt.Errorf("monkey input cut too early")
 	}
-	div, err := strconv.Atoi(strings.TrimPrefix(scanner.Text(), "  Test: divisible by "))
+	div, err := strconv.ParseInt(strings.TrimPrefix(scanner.Text(), "  Test: divisible by "), 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("could not convert divisible to num: %s", err)
 	}
-	res.divisible = div
+	res.divisor = big.NewInt(div)
 
 	if ok := scanner.Scan(); !ok || scanner.Text() == "" {
 		return nil, fmt.Errorf("monkey input cut too early")
@@ -119,34 +142,36 @@ func parseMonkey(scanner *bufio.Scanner) (*monkey, error) {
 	return res, nil
 }
 
-func parseItemsLine(line string) ([]int, error) {
+func parseItemsLine(line string) ([]*big.Int, error) {
 	itemsRaw := strings.Split(strings.TrimPrefix(line, "  Starting items: "), ", ")
-	items := make([]int, len(itemsRaw))
+	items := make([]*big.Int, len(itemsRaw))
 	for i, item := range itemsRaw {
-		num, err := strconv.Atoi(item)
+		num, err := strconv.ParseInt(item, 10, 64)
 		if err != nil {
 			return nil, fmt.Errorf("could not convert starting item %s to num: %s", item, err)
 		}
 
-		items[i] = num
+		items[i] = big.NewInt(num)
 	}
 
 	return items, nil
 }
 
 func simulate(monkeys []*monkey) error {
-	for i := 0; i < 20; i++ {
-		for idx, m := range monkeys {
+	lcm := calcLCM(monkeys)
+
+	for i := 0; i < simulationLen; i++ {
+		for _, m := range monkeys {
 			for len(m.items) > 0 {
-				prevItem := m.items[0]
-				newItem, err := m.performOp()
+				newItem, err := m.performOp(lcm)
 				if err != nil {
 					return err
 				}
-				testRes := newItem%m.divisible == 0
+
+				modRes := &big.Int{}
+				testRes := modRes.Mod(newItem, m.divisor).Cmp(big.NewInt(0)) == 0
 				newIdx := m.nextMonkey[testRes]
 				monkeys[newIdx].appendItem(newItem)
-				fmt.Printf("monkey %d: prev item: %d; new item: %d; new monkey: %d\n", idx, prevItem, newItem, newIdx)
 			}
 		}
 
@@ -157,82 +182,99 @@ func simulate(monkeys []*monkey) error {
 	return nil
 }
 
-func (m *monkey) performOp() (int, error) {
+func (m *monkey) performOp(threshold *big.Int) (*big.Int, error) {
 	if len(m.items) == 0 {
-		return -1, fmt.Errorf("no items left")
+		return &big.Int{}, fmt.Errorf("no items left")
 	}
 
-	res := m.items[0]
+	old := m.items[0]
 	if len(m.items) == 1 {
-		m.items = []int{}
+		m.items = []*big.Int{}
 	} else {
 		m.items = m.items[1:]
 	}
 
-	var first, second int
-	var err error
+	first, second := &big.Int{}, &big.Int{}
 
 	if m.opElems[0] == "old" {
-		first = res
+		first = old
 	} else {
-		first, err = strconv.Atoi(m.opElems[0])
+		raw, err := strconv.ParseInt(m.opElems[0], 10, 64)
 		if err != nil {
-			return -1, fmt.Errorf("could not convert %s to num: %s", m.opElems[0], err)
+			return &big.Int{}, fmt.Errorf("could not convert %s to num: %s", m.opElems[0], err)
 		}
+
+		first = big.NewInt(raw)
 	}
 
 	if m.opElems[2] == "old" {
-		second = res
+		second = old
 	} else {
-		second, err = strconv.Atoi(m.opElems[2])
+		raw, err := strconv.ParseInt(m.opElems[2], 10, 64)
 		if err != nil {
-			return -1, fmt.Errorf("could not convert %s to num: %s", m.opElems[2], err)
+			return &big.Int{}, fmt.Errorf("could not convert %s to num: %s", m.opElems[2], err)
 		}
+
+		second = big.NewInt(raw)
 	}
 
+	res := &big.Int{}
 	switch m.opElems[1] {
 	case "+":
-		res = first + second
+		res.Add(first, second) // pretty unusual, the result is stored in the caller(res)
 	case "-":
-		res = first - second
+		res.Sub(first, second)
 	case "*":
-		res = first * second
+		res.Mul(first, second)
 	case "/":
-		res = first / second
+		res.Div(first, second)
 	default:
-		return -1, fmt.Errorf("invalid operator: %s", m.opElems[1])
+		return &big.Int{}, fmt.Errorf("invalid operator: %s", m.opElems[1])
 	}
 
-	m.opCount++
+	m.opCount.Add(m.opCount, big.NewInt(1))
+	res = res.Mod(res, threshold)
 
-	return res / itemDivider, nil
+	return res, nil
 }
 
-func (m *monkey) appendItem(item int) {
+func (m *monkey) appendItem(item *big.Int) {
 	m.items = append(m.items, item)
 }
 
-func calculateRes(monkeys []*monkey) int {
-	firstMax, secondMax := math.MinInt, math.MinInt
+// LCM means lowest common multiplier, a number which all the participating numbers divide into
+// Since all the divisibility numbers are primes, we simply need to multiply them between each other
+func calcLCM(monkeys []*monkey) *big.Int {
+	res := big.NewInt(1)
+
+	for _, monkey := range monkeys {
+		res = res.Mul(res, monkey.divisor)
+	}
+
+	return res
+}
+
+func calculateRes(monkeys []*monkey) *big.Int {
+	firstMax, secondMax := big.NewInt(0), big.NewInt(0)
 
 	for _, m := range monkeys {
-		if m.opCount > firstMax {
-			if firstMax > secondMax {
+		if m.opCount.Cmp(firstMax) == 1 { // Cmp() => 1 - greater than, 0 - equal, -1 - less than
+			if firstMax.Cmp(secondMax) == 1 {
 				secondMax = firstMax
 			}
 			firstMax = m.opCount
-		} else if m.opCount > secondMax {
+		} else if m.opCount.Cmp(secondMax) == 1 {
 			secondMax = m.opCount
 		}
 	}
 
-	fmt.Printf("top opCounts: %d + %d\n", firstMax, secondMax)
+	fmt.Printf("top opCounts: %d & %d\n", firstMax, secondMax)
 
-	return firstMax * secondMax
+	return firstMax.Mul(firstMax, secondMax)
 }
 
 func printMonkeys(monkeys []*monkey) {
 	for i, m := range monkeys {
-		fmt.Printf("monkey %d: %+v\n", i, m)
+		fmt.Printf("monkey %d: opCount: %v\n", i, *m.opCount)
 	}
 }
